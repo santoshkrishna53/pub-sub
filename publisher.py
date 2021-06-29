7d66c4f2d35b687521c7d59a4c612fc64988bc3c
santoshkrishna53 <35844725+santoshkrishna53@users.noreply.github.com>
2021-06-29 15:00:46 +0530

    pubsub

---
+++ publisher.py
@@ -0,0 +1,28 @@
+from concurrent import futures
+from google.cloud import pubsub_v1
+import os
+import json
+with open('students.json') as f:
+  students_data = json.load(f)
+
+
+project_id = "springmltraining-316807"
+topic_id = "pub-sub-training"
+credential_path = "newpub.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+
+
+def pub(project_id, topic_id):
+    client = pubsub_v1.PublisherClient()
+    topic_path = client.topic_path(project_id, topic_id)
+    data = json.dumps(students_data)
+    print(type(data))
+    message = bytes(data, 'utf-8')
+    api_future = client.publish(topic_path, message)
+    message_id = api_future.result()
+    print(f"Published {data} to {topic_path}: {message_id}")
+
+
+if __name__ == "__main__":
+    pub(project_id, topic_id)
---
+++ sub.py
@@ -0,0 +1,40 @@
+from google.cloud import pubsub_v1
+import os
+import json
+import codecs
+project_id = "springmltraining-316807"
+topic_id = "pub-sub-training"
+sub_id = 'pub-sub-training-sub'
+credential_path = "newpub.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+def sub(project_id, subscription_id, timeout=None):
+    subscriber_client = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber_client.subscription_path(project_id, subscription_id)
+
+    def callback(message):
+        data = str(message.data)
+        with open('output.json', 'wb') as f:
+           json.dump(data, codecs.getwriter('utf-8')(f), ensure_ascii=False)
+        print('message written to file output.json')
+        print(f"Received {message}.")
+       
+        message.ack()
+        print(f"Acknowledged {message.message_id}.")
+
+    streaming_pull_future = subscriber_client.subscribe(
+        subscription_path, callback=callback
+    )
+    print(f"Listening for messages on {subscription_path}..\n")
+
+    try:
+        streaming_pull_future.result(timeout=timeout)
+    except:  
+        streaming_pull_future.cancel()  
+        streaming_pull_future.result() 
+
+    subscriber_client.close()
+
+
+if __name__ == "__main__":
+    sub(project_id, sub_id)
